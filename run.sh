1af8d162472984aaa2220a5f5c121a979c67a7f8
Yogesh, Nakrani <yogeshnakrani9@gmail.com>
2024-07-04 23:40:41 +0530

    pipeline is set for the experimentation, 1. change the feature_generator and dispatcher to experiment with different models, added predict.py to generate the submission.py

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 export TRAINING_DATA=input/train_folds.csv
-export FOLD=0
+export TEST_DATA=input/test.csv
+# export FOLD=3
 export MODEL=$1
 
-python -m src.train
+# FOLD=0 python -m src.train
+# FOLD=1 python -m src.train
+# FOLD=2 python -m src.train
+# FOLD=3 python -m src.train
+# FOLD=4 python -m src.train
+python -m src.predict
--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ import joblib
 from . import dispatcher
 
 TRAINING_DATA = os.environ.get('TRAINING_DATA')
+TEST_DATA = os.environ.get("TEST_DATA")
 FOLD = int(os.environ.get('FOLD'))
 MODEL = os.environ.get('MODEL')
 
@@ -22,34 +23,47 @@ FOLD_MAPPING = {
 
 if __name__ == "__main__":
     df = pd.read_csv(TRAINING_DATA)
-    train_df = df[df.kfold.isin(FOLD_MAPPING[FOLD])]
-    valid_df = df[df.kfold == FOLD]
+    df_test = pd.read_csv(TEST_DATA)
+    train_df = df[df.kfold.isin(FOLD_MAPPING[FOLD])].reset_index(drop=True)    
+    valid_df = df[df.kfold == FOLD].reset_index(drop=True)
 
     ytrain = train_df.target.values
     yvalid = valid_df.target.values
 
-    train_df = train_df[train_df.drop(["id", "target", "kfold"], axis=1).columns]
-    valid_df = valid_df[valid_df.drop(["id", "target", "kfold"], axis=1).columns]
+    train_df = train_df.drop(["id", "target", "kfold"], axis=1)
+    valid_df = valid_df.drop(["id", "target", "kfold"], axis=1)
 
     valid_df = valid_df[train_df.columns]
     
-    label_encoder = []
+    label_encoder = {}
 
     for c in train_df.columns:
         lbl = preprocessing.LabelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        train_df.loc[:, c] = train_df.loc[:, c].astype(str).fillna("NONE")
+        valid_df.loc[:, c] = valid_df.loc[:, c].astype(str).fillna("NONE")    
+        df_test.loc[:, c] = df_test.loc[:, c].astype(str).fillna("NONE")
+
+        lbl.fit(train_df[c].values.tolist() +
+                valid_df[c].values.tolist() +
+                df_test[c].values.tolist()
+                )
 
         train_df[c] = lbl.transform(train_df[c].values.tolist())
         valid_df[c] = lbl.transform(valid_df[c].values.tolist())
+        
+        #df_test transform is only required to collect the encoding features for training
+        # In real world you won't have test data available, You try to make code such that if there is a new category it can handle it
 
-        label_encoder.append((c, lbl))
+        label_encoder[c] = lbl
 
     # data is ready to train
     clf = dispatcher.MODELS[MODEL]
     clf.fit(train_df, ytrain)
     preds = clf.predict_proba(valid_df)[:, 1]
+    print(metrics.roc_auc_score(yvalid, preds))
     print(f"The ROC value of data with fold {FOLD} is : ", metrics.roc_auc_score(yvalid, preds))
 
-    joblib.dump(label_encoder, f"models/{MODEL}_label_encoder.pkl")
-    joblib.dump(clf, f"models/{MODEL}.pkl")
+    joblib.dump(label_encoder, f"models/{MODEL}_{FOLD}_label_encoder.pkl")
+    joblib.dump(clf, f"models/{MODEL}_{FOLD}.pkl")
+    joblib.dump(train_df.columns, f"models/{MODEL}_{FOLD}_columns.pkl")
 
